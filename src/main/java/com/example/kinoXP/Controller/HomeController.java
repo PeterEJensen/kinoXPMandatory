f2ad21518daaac2983a6141b9c7c482f5b7a28a8
DESKTOP-DM517T5\farso <farsofli@gmail.com>
2018-09-27 11:03:47 +0200

    showMovies added Tabel for at sætte film ind er lavet (virker dog ikke)

--- src/main/java/com/example/kinoXP/Controller/HomeController.java
+++ src/main/java/com/example/kinoXP/Controller/HomeController.java
@@ -1,13 +1,12 @@
 package com.example.kinoXP.Controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 @Controller
 public class HomeController {
 
@@ -56,5 +55,12 @@ public String index() {
     public String login() {
         return "login";
     }
+
+
+    @GetMapping("/showMovies")
+    public String showMovies() {
+        return "showMovies";
+    }
+
 }
 
