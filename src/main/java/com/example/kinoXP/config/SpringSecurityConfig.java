5511bf6032e49cbdcc44c951efe77f6864353e19
kableiv <nerditoff@hotmail.com>
2018-09-27 11:13:20 +0200

    Added functionality to store/add/delete and get images from database. Currently not working (Spring is possibly interfering)

--- src/main/java/com/example/kinoXP/config/SpringSecurityConfig.java
+++ src/main/java/com/example/kinoXP/config/SpringSecurityConfig.java
@@ -34,6 +34,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers( "/css/**","/resources/**","/", "/home", "/about", "/index/**", "/register", "/confirm", "/truman", "/eet", "/silencelambs", "/clockwork", "/american","/add/**","/h2-console/**", "/image").permitAll()
+                .antMatchers("/admin/**", "/delete", "/create", "/calendar", "/post", "/contact").hasAnyRole("ADMIN")
+                .antMatchers("/user/**", "/calendar", "/search", "/search/**", "/contact").hasAnyRole("USER")
                 .antMatchers("/", "/home", "/about", "/index/**", "/register", "/confirm", "/truman", "/eet", "/silencelambs", "/clockwork", "/american","/add/**","/h2-console/**", "/css/**", "/js/**").permitAll()
                 .antMatchers().hasAnyRole("ADMIN")
                 .antMatchers().hasAnyRole("USER")
