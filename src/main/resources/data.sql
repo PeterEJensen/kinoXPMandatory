61eebff2f4e46163f2d78ef1ccf7c0e2e6f3b503
rasmushecter <rasmushecter@gmail.com>
2018-10-04 09:07:33 +0200

    add data.sql with hard coded movies for database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,30 @@
 INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
-VALUES ('dsfwefwesf.eargadf.dgdsfgdsfg.dfgdfged.efesfesfaer.fgesfae.feafefrfergrdfg.adgdzg','Drama', 'American Beauty');
+VALUES ('A sexually frustrated suburban father has a mid-life crisis after becoming infatuated with his daughters best friend.','Drama', 'American Beauty');
 
 INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
-VALUES(timestamp '2017-10-12 21:00:00','1');
+VALUES(timestamp '2017-10-12 10:00:00','1');
 
 INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
-VALUES ('ffsdfsdfsdfsfsdfsdfdfrtgadfggrtb cczskdfekrgdobfcvd<kse<knwefdvderf.evdfbfgbv.esfwesfeferbrttyntyfgzx.','Horor', 'Clockwork Orange');
+VALUES ('In the future, a sadistic gang leader is imprisoned and volunteers for a conduct-aversion experiment, but it doesnt go as planned.','Horor', 'Clockwork Orange');
 
 INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
-VALUES(timestamp '2016-10-11 23:59:23','2');
+VALUES(timestamp '2016-10-11 23:59:00','2');
 
 INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
-VALUES ('ftgherafergrstdgrgerg.rgrtgrthtyhgfgadrswef.erdgergrst.gdr','Science Fiction', 'E.T. - The Exstra-terrestrial');
+VALUES ('A troubled child summons the courage to help a friendly alien escape Earth and return to his home world.','Science Fiction', 'E.T. - The Exstra-terrestrial');
 
 INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
 VALUES(timestamp '2016-10-11 20:00:00','3');
 
 INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
-VALUES ('4wtrergrtgtrhrgferfer.ergere.rgerrfe4  erferear.fer efe.ferf ','Horror', 'The Silence of The Lambs');
+VALUES ('A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.','Horror', 'The Silence of The Lambs');
 
 INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
-VALUES(timestamp '2016-10-11 22:22:23','4');
+VALUES(timestamp '2016-10-11 21:00:00','4');
 
 
 INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
-VALUES ('rtgdxcftdgerferfef.rgredverferfefer.regerdverdfesf.erdfeswe','Comedy', 'The Truman Show');
+VALUES ('An insurance salesman/adjuster discovers his entire life is actually a television show.','Comedy', 'The Truman Show');
 
 INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
-VALUES(timestamp '2016-10-11 22:22:23','5');
+VALUES(timestamp '2016-10-11 18:00:00','5');
