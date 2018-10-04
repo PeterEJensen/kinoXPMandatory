1c7e65808f4a0b2c89d0c4179debeefbb9f89f0a
rasmushecter <rasmushecter@gmail.com>
2018-10-04 08:47:44 +0200

    add data.sql with hard coded movies for database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
+VALUES ('dsfwefwesf.eargadf.dgdsfgdsfg.dfgdfged.efesfesfaer.fgesfae.feafefrfergrdfg.adgdzg','Drama', 'American Beauty');
+
+INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
+VALUES(timestamp '2017-10-12 21:00:00','1');
+
+INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
+VALUES ('ffsdfsdfsdfsfsdfsdfdfrtgadfggrtb cczskdfekrgdobfcvd<kse<knwefdvderf.evdfbfgbv.esfwesfeferbrttyntyfgzx.','Horor', 'Clockwork Orange');
+
+INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
+VALUES(timestamp '2016-10-11 23:59:23','2');
+
+INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
+VALUES ('ftgherafergrstdgrgerg.rgrtgrthtyhgfgadrswef.erdgergrst.gdr','Science Fiction', 'E.T. - The Exstra-terrestrial');
+
+INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
+VALUES(timestamp '2016-10-11 20:00:00','3');
+
+INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
+VALUES ('4wtrergrtgtrhrgferfer.ergere.rgerrfe4  erferear.fer efe.ferf ','Horror', 'The Silence of The Lambs');
+
+INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
+VALUES(timestamp '2016-10-11 22:22:23','4');
+
+
+INSERT INTO MOVIE(DESCRIPTION,GENRE,TITLE)
+VALUES ('rtgdxcftdgerferfef.rgredverferfefer.regerdverdfesf.erdfeswe','Comedy', 'The Truman Show');
+
+INSERT INTO MOVIE_SHOWING(DATE,MOVIE_ID)
+VALUES(timestamp '2016-10-11 22:22:23','5');
